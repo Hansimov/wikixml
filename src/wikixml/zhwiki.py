fea8173d63fc0a1988d693f1b0ef6d32d74b0fef
Hansimov <591172499@qq.com>
2024-11-19 23:57:25 +0800

    :zap: [Enhance] ZhWikiBz2Parser: replace xml with lxml, and enable complete page extraction with ElementToDictConverter

--- src/wikixml/zhwiki.py
+++ src/wikixml/zhwiki.py
@@ -1,16 +1,20 @@
 import bz2
 
+from lxml import etree
 from pathlib import Path
-from tclogger import logger, TCLogbar, chars_slice
+from tclogger import logger, logstr, TCLogbar
+from tclogger import dict_to_str, dict_get, chars_len, chars_slice, brk
 from typing import Union
-from xml.etree.ElementTree import iterparse
+
+from .structures import ElementToDictConverter
 
 
 class ZhWikiBz2Parser:
     def __init__(self, file_path: Union[str, Path]):
         self.file_path = file_path
-        self.xmlns = "http://www.mediawiki.org/xml/export-0.11/"
+        self.xmlns = "{http://www.mediawiki.org/xml/export-0.11/}"
         self.logbar = TCLogbar()
+        self.converter = ElementToDictConverter()
 
     def preview_lines(self, max_lines: int = 10):
         with bz2.BZ2File(self.file_path, "rb") as rf:
@@ -22,22 +26,14 @@ class ZhWikiBz2Parser:
 
     def preview_pages(self, max_pages: int = None):
         with bz2.BZ2File(self.file_path, "rb") as rf:
-            context = iterparse(rf, events=("start", "end"))
-            _, root = next(context)
-            page_count = 0
-            for idx, (event, element) in enumerate(context):
-                if event == "end" and element.tag == f"{{{self.xmlns}}}page":
-                    title = element.findtext(f"{{{self.xmlns}}}title")
-                    text = element.findtext(
-                        f"{{{self.xmlns}}}revision/{{{self.xmlns}}}text"
-                    )
-                    title_part = chars_slice(title, end=20)
-                    if title:
-                        self.logbar.update(increment=1, desc=title_part)
-                    page_count += 1
-                    if max_pages is not None and page_count >= max_pages:
-                        break
-                if idx % 10000 == 0:
-                    root.clear()
-            title_part = chars_slice(title, end=20)
-            self.logbar.update(flush=True, desc=title_part)
+            context = etree.iterparse(rf, tag=self.xmlns + "page")
+            for idx, (_, element) in enumerate(context):
+                if max_pages and idx >= max_pages:
+                    break
+                page_dict = self.converter.convert(element)
+                # logger.mesg(dict_to_str(page_dict))
+                title = dict_get(page_dict, "title")
+                title_part = chars_slice(title, 0, 20)
+                text = dict_get(page_dict, "revision.text")
+                text_len = chars_len(text)
+                logger.note(f"* {title_part} : {logstr.mesg(brk(text_len))}")
